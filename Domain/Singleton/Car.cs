d53c4f14ccfa5acff89a0265571c4d35253e6f35
Nilson Rosado <nilsonrosado007@gmail.com>
2020-05-15 08:03:46 -0500

    Se añade Singleton

---
+++ Domain/Singleton/Car.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain.Singleton
+{
+    public class Car
+    {
+        private static Car _instance;
+        public string model { get; set; }
+
+        public Car()
+        {
+        }
+
+        public static Car GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Car();
+            }
+
+            return _instance;
+        }
+
+        public string GetModel()
+        {
+            return model;
+        }
+
+    }
+}
